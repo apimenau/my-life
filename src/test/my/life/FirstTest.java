43e7a80e75239eff8e14be121f3b0dc9dde829e2
andrei_pimenau <andrei_pimenau@epam.com>
2012-11-28 12:42:36 +0300

    initial commit

---
+++ src/test/my/life/FirstTest.java
@@ -0,0 +1,13 @@
+package test.my.life;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrei_Pimenau
+ * Date: 28.11.12
+ * Time: 4.49
+ * To change this template use File | Settings | File Templates.
+ */
+public class FirstTest {
+
+
+}
